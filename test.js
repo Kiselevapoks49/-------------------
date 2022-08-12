a208fea6df788733d4e3c53622c0ddaa3b64bf97
Kiselevapoks49 <alen05kiseleva@yandex.ru>
2022-08-12 16:46:01 +0300

    commit 12.08.22

--- test.js
+++ test.js
@@ -1,106 +1,138 @@
-function handleSubmit(event) {
-    event.preventDefault();
+  var defects = document.getElementById('defects');
+  
+  function GetDefects() {
+  
+  var url = "http://10.15.101.91/test/hs/CentralService/defects";
 
-    const data = new FormData(event.target);
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+  xhr.setRequestHeader("Authorization", "Basic SFM6ZkE1Y2F0dXE=");
+  
+  xhr.onreadystatechange = function () {
+     if (xhr.readyState === 4) {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+        
+        myObj = [];
+        myObj = JSON.parse(xhr.responseText);
+              txt = "<select>"
+              for (xhr.responseText in myObj) {
+                   txt += "<option>" + myObj.name;
+              }
+              txt += "</select>" 
+              document.getElementById("defect").innerHTML = txt;
 
-    const value = Object.fromEntries(data.entries());
-
-    console.log({ value });
-  }
-
-  const form = document.querySelector('form');
-  form.addEventListener('submit', handleSubmit);
-
-
-var url = "http://10.15.101.91/test/hs/CentralService/Get_oborudovanie ";
-
-var xhr = new XMLHttpRequest();
-xhr.open("POST", url);
-
-xhr.setRequestHeader("Authorization", "Basic QmFzaWM6U0ZNNlprRTFZMkYwZFhFPQ==");
-xhr.setRequestHeader("Content-Type", "application/json");
-
-xhr.onreadystatechange = function () {
-   if (xhr.readyState === 4) {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
-   }};
-
-var nameOborudovanie = `{
-    "number": "здесь или часть наименования оборудования или инвентарного номера"
-    }
+    }};
     
-`;
+   xhr.send();}
 
-xhr.send(nameOborudovanie);
+   //оборудование
 
-var url = "http://10.15.101.91/test/hs/CentralService/defects  ";
+   function GetOborudovanie() {
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", url);
+   var url = "http://10.15.101.91/test/hs/CentralService/Get_oborudovanie";
+   var xhr = new XMLHttpRequest();
+   xhr.open("POST", url);
 
-xhr.setRequestHeader("Authorization", "Basic QmFzaWM6U0ZNNlprRTFZMkYwZFhFPQ==");
+   xhr.setRequestHeader("Authorization", "Basic SFM6ZkE1Y2F0dXE=");
+   xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+   xhr.setRequestHeader("Content-Type", "application/json");
 
-xhr.onreadystatechange = function () {
+   xhr.onreadystatechange = function () {
    if (xhr.readyState === 4) {
       console.log(xhr.status);
       console.log(xhr.responseText);
    }};
 
-xhr.send();
+   var data = `{
+   "number": "155"
+    }`;
 
-var url = "http://10.15.101.91/test/hs/CentralService/GetSotrudnik ";
+   xhr.send(data);}
 
-var xhr = new XMLHttpRequest();
-xhr.open("POST", url);
+   //сотрудники 
 
-xhr.setRequestHeader("Authorization", "Basic QmFzaWM6U0ZNNlprRTFZMkYwZFhFPQ==");
-xhr.setRequestHeader("Content-Type", "application/json");
+   function GetSotrudnik () {
 
-xhr.onreadystatechange = function () {
+   var url = "http://10.15.101.91/test/hs/CentralService/GetSotrudnik";
+   var xhr = new XMLHttpRequest();
+   xhr.open("POST", url);
+
+   xhr.setRequestHeader("Authorization", "Basic SFM6ZkE1Y2F0dXE=");
+   xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+   xhr.setRequestHeader("Content-Type", "application/json");
+
+   xhr.onreadystatechange = function () {
    if (xhr.readyState === 4) {
       console.log(xhr.status);
       console.log(xhr.responseText);
    }};
 
-var data = `{
-  "name": "лома"
-}
+   var data = `{
+   "name": "ива"
+    }`;
 
-`;
+   xhr.send(data);}
 
-xhr.send(data);
 
-var url = "http://10.15.101.91/test/hs/CentralService/insertDef  ";
 
-var xhr = new XMLHttpRequest();
-xhr.open("POST", url);
 
-xhr.setRequestHeader("Authorization", "Basic QmFzaWM6U0ZNNlprRTFZMkYwZFhFPQ==");
-xhr.setRequestHeader("Content-Type", "application/json");
 
-xhr.onreadystatechange = function () {
-   if (xhr.readyState === 4) {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
-   }};
 
-var data = `{
-  "nameOborudovanie": "Машина листогибочная Sahinler RM 1550-90",
-  "vidDef": "Электрический",
-  "podrazdelenie": [
-    "Служба энергетика"
-  ],
-  "opisanie": "Тестовое описание дефекта",
-  "komment": "Тестовый комментарий дефекта",
-  "otvetstv": "659d0fac-a00b-11ea-8177-0cc47a594943",
-  "PK": "SA00",
-  "userPK": "lomanov_ds",
-  "Guid": "fcd38171-703b-11eb-8eb4-0cc47a594943"
-}
 
-`;
 
-xhr.send(data);
+
+
+
+
+
+
+
+
+
+   //заявка 
+
+   //var url = "http://10.15.101.91/test/hs/CentralService/insertDef";
+   //var xhr = new XMLHttpRequest();
+   //xhr.open("POST", url);
+
+   //xhr.setRequestHeader("Authorization", "Basic SFM6ZkE1Y2F0dXE=");
+   //xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+   //xhr.setRequestHeader("Content-Type", "application/json");
+
+   //xhr.onreadystatechange = function () {
+   //if (xhr.readyState === 4) {
+      //console.log(xhr.status);
+      //console.log(xhr.responseText);
+
+      //if (this.status != 200) {
+       // console.log(this.statusText);
+        //return;
+      //} else {
+   //}}};
+
+   //var data = `{"nameOborudovanie": "Машина листогибочная Sahinler RM 1550-90",
+   //"vidDef": "Электрический",
+   //"podrazdelenie": [
+   //  "Служба энергетика"
+   //],
+   //"opisanie": "Тестовое описание дефекта",
+   //"komment": "Тестовый комментарий дефекта",
+   //"otvetstv": "659d0fac-a00b-11ea-8177-0cc47a594943",
+   //"PK": "SA00",
+   //"userPK": "lomanov_ds",
+   //"Guid": "fcd38171-703b-11eb-8eb4-0cc47a594943"
+  //}`;
+
+   
+
+   //xhr.send(data);
+
+
+
+   
 
 
